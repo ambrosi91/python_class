1ff785edb2263d006ce3d203ba025a839867bd87
somename <a@gmail.com>
2022-10-11 16:01:53 +0000

    added class 5

---
+++ class5/function_caller.py
@@ -0,0 +1,20 @@
+#import statement
+from first_function import greetings
+from math_function import multiplication
+from math_function import addition
+from math_function import division
+from math_function import substraction
+import full_function
+from converter import *
+from age_calculator import game
+
+greetings("Andrei")
+multiplication(4, 5)
+addition(1, 2)
+substraction(4, 2)
+division(10, 2)
+full_function.function1()
+FRUIT = ("apple", "pear")
+convert_to_list(FRUIT)
+convert_to_string(10)
+game()
